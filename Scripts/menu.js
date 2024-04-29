3e45d7153e2ce7c23e034c7217398960f2091ea0
CayCall <131669034+CayCall@users.noreply.github.com>
2024-04-29 02:08:20 +0200

    Update menu.js

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,10 +1,10 @@
 const menuItems = [
-    {name: "Home", href: "WSOA3028A/index.html"},
-    {name: "Blog", href: "WSOA3028A/Pages/Blogposts/blogposts.html"},
-    {name: "Essays", href: "WSOA3028A/Pages/Essays/essays.html"},
-    {name: "Portfolio", href: "WSOA3028A/Pages/Portfolio/portfolio.html"},
-    {name: "Design", href: "WSOA3028A/Pages/Web Design/design.html"},
-    {name: "About", href: "WSOA3028A/Pages/About me/about.html"},
+    {name: "Home", href: "WSOA3028A_2565053/index.html"},
+    {name: "Blog", href: "WSOA3028A_2565053/Pages/Blogposts/blogposts.html"},
+    {name: "Essays", href: "WSOA3028A_2565053/Pages/Essays/essays.html"},
+    {name: "Portfolio", href: "WSOA3028A_2565053/Pages/Portfolio/portfolio.html"},
+    {name: "Design", href: "WSOA3028A_2565053/Pages/Web Design/design.html"},
+    {name: "About", href: "WSOA3028A_2565053/Pages/About me/about.html"},
 ];
 export function initialise(currentPage){
 
