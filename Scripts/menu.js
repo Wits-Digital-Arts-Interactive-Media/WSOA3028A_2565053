de85ea3fe1c1e3c0eaf373bba20529f345ea9238
CayCall <131669034+CayCall@users.noreply.github.com>
2024-05-08 20:50:15 +0200

    changes

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,24 +1,37 @@
 const menuItems = [
-    {name:"Home", href: "index.html" },
-    {name:"Blogs", href: "/WSOA3028A_2565053/Pages/Blogposts/blogposts.html"},
-    {name:"Essays" , href: "/WSOA3028A_2565053/Pages/Essays/essays.html" },
-    {name:"Portfolio" , href: "/WSOA3028A_2565053/Pages/Portfolio/portfolio.html"},
-    {name:"Design", href:  "/WSOA3028A_2565053/Pages/Web Design/design.html"},
-    {name:"About", href:  "/WSOA3028A_2565053/Pages/About me/about.html"}
+    { name: "Home", href: "index.html" },
+    { name: "Blogs", href: "/Pages/Blogposts/blogposts.html" },
+    { name: "Essays", href: "/Pages/Essays/essays.html" },
+    { name: "Portfolio", href: "/Pages/Portfolio/portfolio.html" },
+    { name: "Web Design", href: "/Pages/Web%20Design/design.html" },
+    { name: "About", href: "/Pages/About%20me/about.html" }
 ];
 
 export function initialise(currentPage) {
-    const nav = document.querySelector("header > nav")       
-    const ul = document.createElement("ul")                  
-    for (let menuItem of menuItems) {                        
-        const li = document.createElement("li")              
-        if (currentPage != menuItem.name) {                  
-            const a = document.createElement("a")            
-            a.innerText = menuItem.name                      
-            a.setAttribute("href", menuItem.href)                
-            li.appendChild(a)                                
-        } else { li.innerText = menuItem.name }
-        ul.appendChild(li)                                   
+    
+    if (document.querySelector("header > nav > ul")) {
+        return; 
     }
-    nav.appendChild(ul)                                     
+
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li");
+        if (currentPage !== menuItem.name) {
+            const a = document.createElement("a");
+            a.innerText = menuItem.name;
+            a.setAttribute("href", menuItem.href);
+            li.appendChild(a);
+        } else {
+            li.innerText = menuItem.name;
+        }
+        ul.appendChild(li);
+    }
+    nav.appendChild(ul);
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+   
+    const currentPage = document.title;
+    initialise(currentPage);
+});
