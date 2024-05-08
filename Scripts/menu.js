667f00e4ad42954c198c0c7c9f72fd8e036f58ec
CayCall <131669034+CayCall@users.noreply.github.com>
2024-05-08 20:06:08 +0200

    Working

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,37 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('domloaded');
-    // Function to create and append navigation elements
-    function createNavigation() {
-        const nav = document.getElementById('navList');
+const menuItems = [
+    {name:"Home", href: "index.html" },
+    {name:"Blogs", href: "/WSOA3028A_2565053/Pages/Blogposts/blogposts.html"},
+    {name:"Essays" , href: "/WSOA3028A_2565053/Pages/Essays/essays.html" },
+    {name:"Portfolio" , href: "/WSOA3028A_2565053/Pages/Portfolio/portfolio.html"},
+    {name:"Design", href:  "/WSOA3028A_2565053/Pages/Web Design/design.html"},
+    {name:"About", href:  "/WSOA3028A_2565053/Pages/About me/about.html"}
+];
 
-        // Define navigation items
-        const navItems = [
-            { text: "Home", href: "index.html" },
-            { text: "Blog", href: "Pages/Blogposts/blogposts.html" },
-            { text: "Essays", href: "Pages/Essays/essays.html" },
-            { text: "Portfolio", href: "Pages/Portfolio/portfolio.html" },
-            { text: "Design", href: "Pages/Web Design/design.html" },
-            { text: "About-me", href: "Pages/About me/about.html" }
-        ];
-
-        // Create and append navigation elements
-        navItems.forEach(item => {
-            const li = document.createElement('li');
-            const a = document.createElement('a');
-            a.textContent = item.text;
-            a.href = item.href;
-            li.appendChild(a);
-            nav.appendChild(li);
-
-            a.addEventListener('mouseover', () => {
-                a.style.fontSize = '1.2em';
-            });
-            a.addEventListener('mouseout', () => {
-                a.style.fontSize = '1em';
-            });
-        });
+export function initialise(currentPage) {
+    const nav = document.querySelector("header > nav")       
+    const ul = document.createElement("ul")                  
+    for (let menuItem of menuItems) {                        
+        const li = document.createElement("li")              
+        if (currentPage != menuItem.name) {                  
+            const a = document.createElement("a")            
+            a.innerText = menuItem.name                      
+            a.setAttribute("href", menuItem.href)                
+            li.appendChild(a)                                
+        } else { li.innerText = menuItem.name }
+        ul.appendChild(li)                                   
     }
-
-    // Call createNavigation function when DOM is loaded
-    createNavigation();
-});
+    nav.appendChild(ul)                                     
+}
