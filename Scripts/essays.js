4115f43d8ed8718876e831f93680f5447421a64e
CayCall <131669034+CayCall@users.noreply.github.com>
2024-05-25 23:38:06 +0200

    finished essay page

---
+++ Scripts/essays.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.querySelector('.btn-variation-2');
+    
+    downloadButton.addEventListener('click', function() {
+        // Replace the URL with the actual path to your essay PDF
+        const essayUrl = 'collection/WSOA3028A_2565053_UI & UX Analysis.pdf';
+        const link = document.createElement('a');
+        link.href = essayUrl;
+        link.download = 'WSOA3028A_2565053_UI & UX Analysis.pdf'; // The name of the file to be downloaded
+        link.click();
+    });
+});
