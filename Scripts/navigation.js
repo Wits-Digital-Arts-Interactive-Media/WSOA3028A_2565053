86c72bcc1d5cd1ed7e177743d3502fa65ae4b454
CayCall <131669034+CayCall@users.noreply.github.com>
2024-05-26 20:38:34 +0200

    Design Page finished styling

---
+++ Scripts/navigation.js
@@ -0,0 +1,16 @@
+const root = "/WSOA3028A_2565053";
+
+function loadIteration1Page() {
+    const Iteration1Url = root + "/Pages/Web Design/iteration1.html";
+    window.location.href = Iteration1Url;
+}
+  
+function loadIteration2Page() {
+    const EssayUrl = root + "/Pages/Essays/essays.html";
+    window.location.href = EssayUrl;
+}
+  
+function loadIteration3Page() {
+    const DesignUrl = root + "/Pages/Web Design/design.html";
+    window.location.href = DesignUrl;
+}
