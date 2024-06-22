c1fb3446d0db92ee49c0acc9fe292b391e09e092
CayCall <131669034+CayCall@users.noreply.github.com>
2024-06-22 22:09:15 +0200

    LATEST CHANGES

--- Scripts/portfolio.js
+++ Scripts/portfolio.js
@@ -1,6 +1,9 @@
+<<<<<<< HEAD
+function downloadGame(url) {
+=======
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('Game-1-btn').addEventListener('click', function() {
-        downloadFile('WSOA3028A_2565053/Pages/Portfolio/Game Builds/WSOA3003A_2565053_FinalExam.zip', 'WSOA3003A_2565053_FinalExam.zip');
+    document.getElementById('Game-1').addEventListener('click', function() {
+        downloadFile('WSOA3028A_2565053/Pages/Portfolio/Game Builds/WSOA3003A_2565053_FinalExam', 'WSOA3003A_2565053_FinalExam');
     });
 
     document.getElementById('Game-2').addEventListener('click', function() {
@@ -9,9 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function downloadFile(url, filename) {
+>>>>>>> parent of 01fa41e (touched up my blogs page and added my game files)
     const link = document.createElement('a');
     link.href = url;
-    link.download = filename;
+    link.download = '';
+    document.body.appendChild(link);
     link.click();
-    console.log("worked");
+    document.body.removeChild(link);
 }
